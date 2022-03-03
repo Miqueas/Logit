851d394f1d4057e67e4b947de1da967318c368f1
Miqueas <miqueas2020@yahoo.com>
2022-03-03 15:44:55 +0000

    Nim: minor chnages in test

--- Test.nim
+++ Test.nim
@@ -1,4 +1,4 @@
-import Logit
+import logit
 import std/os
 
 let logger = newLogit(path = getCurrentDir(), name = "Test", console = true)
@@ -9,5 +9,7 @@ logger.log(DEBUG, "D")
 logger.log(INFO, "I")
 logger.log(WARN, "W")
 logger.header("hello")
+logger.expect(29 < 30, "Expected 29 < 30")
 logger.log(ERROR, "E")
+# Never runs
 logger.log(FATAL, "F")
