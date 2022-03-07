7834a218689bd89284e03e93fd614fb871fe6898
Miqueas <miqueas2020@yahoo.com>
2022-03-07 18:34:47 +0000

    Lua: updated test

--- tests/test.lua
+++ tests/test.lua
@@ -1,8 +1,22 @@
-local Logit  = require("logit")
-local logger = Logit("./", "Test", nil, true)
+local logit = require("logit")
+local logger = logit:new("./", "Test", nil, true)
 
-logger(2, "Log library started!")
-logger(3, "This is a debug message")
+logger(logit.OTHER, "O")
+logger(logit.OTHER)
+logger(logit.TRACE, "T")
+logger(logit.TRACE)
+logger(logit.DEBUG, "D")
+logger(logit.DEBUG)
+logger(logit.INFO, "I")
+logger(logit.INFO)
+logger(logit.WARN, "W")
+logger(logit.WARN)
 
-logger:expect(true)
-logger:expect(false, "E")
+logger:header("hello there, this is a header :p")
+logger:expect(2 < 4, "2 isn't minor than 4!!!!!!!!")
+
+logger(logit.ERROR, "E")
+-- Never runs
+logger(logit.ERROR)
+logger(logit.FATAL, "F")
+logger(logit.FATAL)
