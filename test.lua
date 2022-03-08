91e14875eab76f6bf462d57d94ecc022141653f7
Miqueas <miqueas2020@yahoo.com>
2022-03-08 17:55:37 +0000

    Moved tests

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 local logit = require("logit")
-local logger = logit:new("./", "Test", nil, true)
+local logger = logit:new("./", "TEST", nil, true)
 
 logger(logit.OTHER, "O")
 logger(logit.OTHER)
@@ -15,8 +15,8 @@ logger(logit.WARN)
 logger:header("hello there, this is a header :p")
 logger:expect(2 < 4, "2 isn't minor than 4!!!!!!!!")
 
-logger(logit.ERROR, "E")
+--logger(logit.ERROR, "E")
 -- Never runs
-logger(logit.ERROR)
+--logger(logit.ERROR)
 logger(logit.FATAL, "F")
 logger(logit.FATAL)
