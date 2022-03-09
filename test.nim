09071ea7774eabfed1c980974bee3598047b65eb
Miqueas <miqueas2020@yahoo.com>
2022-03-09 18:02:01 +0000

    Nim: polished API
      * API changes:
        * Now using an internal file to prevent spaming system calls
        * Added `prepare` proc to allow using existing handmade `Logit`
          instances
        * `initLogit` now is just for create new `Logit` instances with
          custom properties values, it will keep unset properties to
          their default values
        * Added `done` proc to close the internal file
      * Internal changes:
        * Updated the code to use the internal file
        * Documented a bit the code

--- test.nim
+++ test.nim
@@ -1,18 +1,19 @@
 import logit
-from std/os import getCurrentDir
+from std/os import getCurrentDir, getTempDir
 
-let logger = newLogit(path = getCurrentDir(), name = "TEST", console = true)
+var logger = initLogit(getCurrentDir(), "TEST", console = true, exit = false)
 
 # Uses default logging level
 logger("hello!")
 logger(TRACE, "this seems to be working fine :)")
 logger(INFO, "this is an info message")
-logger(DEBUG, "🔥")
-logger(WARN, "WARNING!!!!")
+logger(DEBUG, "this is a debug message")
+logger(WARN, "be careful 🔥")
 
 logger.header("hello there, this is a header :p")
-logger.expect(29 < 30, "Expected 29 < 30")
+logger.expect(29 < 30, "Expected 29 > 30")
 
-logger(ERROR, "E")
+logger(ERROR, "something went wrong!!!")
 # Never runs
-logger(FATAL, "F")
+logger(FATAL, "O H   M Y   G O D N E S S")
+logger.done()
