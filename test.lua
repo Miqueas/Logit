55c4ec51cdf1bbe0f387e8b4a86849c1d7f4022e
Miqueas <miqueas2020@yahoo.com>
2023-03-30 08:48:20 -0500

    Updated tests

--- test.lua
+++ test.lua
@@ -1,6 +1,6 @@
 local logit = require("logit")
 
-local log = logit:new("./", "TEST", nil, true, false)
+local log = logit:new("./", "Test", nil, true, false)
 
 log(nil, "hello!")
 log(logit.TRACE, "this seems to be working fine :)")
@@ -8,9 +8,12 @@ log(logit.INFO, "this is an info message")
 log(logit.DEBUG, "this is a debug message")
 log(logit.WARN, "be careful 🔥")
 
-log:header("hello there, this is a header :p")
-log:expect(29 > 30, "expected 29 > 30")
-log:expect(false, "expected `true`", logit.FATAL)
+log:header("hello there, this is a header :P")
+
+-- Checks if 'e' exists
+log:test(5 > 6, "5 > 6 == true")
+log:test(e, "'e' doesn't exists", logit.FATAL)
+log:test()
 
 log(logit.ERROR, "something went wrong!!!")
 log(logit.FATAL, "O H   M Y   G O D N E S S")
--- test.nim
+++ test.nim
@@ -1,19 +1,26 @@
-import logit
+import ./logit
 from std/os import getCurrentDir
 
 var log = initLogit(getCurrentDir(), "TEST", console = true, exit = false)
 
 # Uses default logging level
 log("hello!")
+# Uses the given logging level
 log(TRACE, "this seems to be working fine :)")
 log(INFO, "this is an info message")
 log(DEBUG, "this is a debug message")
 log(WARN, "be careful 🔥")
 
+# You can also do this
+"hi there!".log()
+TRACE.log("tracing data...")
+WARN.log("something malicious is brewing 🤨")
+
 log.header("hello there, this is a header :p")
-log.expect(29 > 30, "expected 29 > 30")
-log.expect(false, "expected `true`", lvl = FATAL)
+log.test(29 > 30, "expected 29 > 30")
+log.test(false, "expected `true`", lvl = FATAL)
 
 log(ERROR, "something went wrong!!!")
 log(FATAL, "O H   M Y   G O D N E S S")
+
 log.done()
