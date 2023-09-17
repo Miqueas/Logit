44329c43a6342b09c8880c0b19ebb04ec8cddc4c
Miqueas <miqueas2020@yahoo.com>
2023-09-16 21:42:28 -0500

    Refactorized Nim code. No API breaking changes with this commit.

--- logit.nim
+++ logit.nim
@@ -6,46 +6,13 @@
   Git Repository: https://github.com/Miqueas/Logit
 ]#
 
-import std/times
-import std/strformat
-from std/os import dirExists, getTempDir, getAppFilename, `/`
-from std/strutils import join, format
-
-#[======== PRIVATE ========]#
-
-proc e(n: varargs[int]): string = return '\e' & '[' & join(n, ";") & 'm'
-
-const
-  FMT = (
-    Filename: "$1_$2.log",
-    Time: "HH:mm:ss",
-    Out: (
-      File: "$1 [$2 $3] $4:$5 $6\n",
-      Console: fmt"{e(2)}$1 [{e(0,1)}$2 $3$4{e(0,2)}] $5:$6{e(0)} $7"
-      #                                 ^~ This one is used for the log level color
-    ),
-    Header: (
-      File: "\n$1 [$2]\n\n",
-      Console: '\n' & fmt"{e(2)}$1 [{e(0, 1)}$2{e(0, 2)}]{e(0)}" & '\n'
-    ),
-    Quit: (
-      File: "$1 [EXIT]\n",
-      Console: fmt"{e(2)}$1 [{e(0, 1)}$2EXIT{e(0, 2)}]{e(0)}"
-      #                               ^~ This one is used for the log level color
-    )
-  )
-
-  ASSOC = [
-    ( name: "OTHER", color: 30 ),
-    ( name: "TRACE", color: 32 ),
-    ( name: "INFO.", color: 34 ),
-    ( name: "DEBUG", color: 36 ),
-    ( name: "WARN.", color: 33 ),
-    ( name: "ERROR", color: 31 ),
-    ( name: "FATAL", color: 35 )
-  ]
-
-#[======== PUBLIC ========]#
+import std/[
+  os,
+  times,
+  tables,
+  strutils,
+  strformat,
+]
 
 type
   LogLevel* = enum
@@ -66,6 +33,53 @@ type
     defaultLevel*: LogLevel
     enableConsole*: bool
 
+proc e(n: varargs[int]): string = return '\e' & '[' & join(n, ";") & 'm'
+
+proc `$`*(lvl: LogLevel): string =
+  return case lvl:
+    of OTHER: "OTHER"
+    of TRACE: "TRACE"
+    of INFO: "INFO."
+    of DEBUG: "DEBUG"
+    of WARN: "WARN."
+    of ERROR: "ERROR"
+    of FATAL: "FATAL"
+
+const
+  fmt = (
+    fileName: "$1_$2.log",
+    time: "HH:mm:ss",
+    fileLine: "$1 [$2 $3] $4:$5 $6\n",
+    consoleLine: fmt"{e(2)}$1 [{e(0,1)}$2 $3$4{e(0,2)}] $5:$6{e(0)} $7",
+    #                                    ^~ This one is used for the log level color
+    fileHeader: "\n$1 [$2]\n\n",
+    consoleHeader: '\n' & fmt"{e(2)}$1 [{e(0, 1)}$2{e(0, 2)}]{e(0)}" & '\n',
+    fileExit: "$1 [EXIT]\n",
+    consoleExit: fmt"{e(2)}$1 [{e(0, 1)}$2EXIT{e(0, 2)}]{e(0)}",
+    #                                   ^~ This one is used for the log level color
+  )
+
+  assoc = toTable({
+    OTHER: 30,
+    TRACE: 32,
+    INFO: 34,
+    DEBUG: 36,
+    WARN: 33,
+    ERROR: 31,
+    FATAL: 35,
+  })
+#[
+  assoc = [
+    ( name: "OTHER", color: 30 ),
+    ( name: "TRACE", color: 32 ),
+    ( name: "INFO.", color: 34 ),
+    ( name: "DEBUG", color: 36 ),
+    ( name: "WARN.", color: 33 ),
+    ( name: "ERROR", color: 31 ),
+    ( name: "FATAL", color: 35 )
+  ]
+]#
+
 # Prepares Logit for logging using the given `Logit` instance.
 # This function assumes that `Logit` has everything ready to
 # start logging, that means you must have set the `path` property
@@ -73,7 +87,7 @@ proc prepare*(self: var Logit) {.raises: [IOError, ValueError].} =
   let
     dt = now()
     date = dt.format(self.filePrefix)
-    filename = FMT.Filename.format(date, self.namespace)
+    filename = fmt.fileName.format(date, self.namespace)
 
   try:
     self.file = open(self.path / filename, fmAppend)
@@ -108,38 +122,23 @@ proc initLogit*(path = getTempDir(),
 # Logging API
 template log*(self: Logit, lvl: LogLevel, logMsg = "", quitMsg = "") =
   let
-    time = now().format(FMT.Time)
+    time = now().format(fmt.time)
     msg =
-      if logMsg == "": ASSOC[ord(lvl)].name
+      if logMsg == "": $lvl
       else: logMsg
     info = instantiationInfo(0)
 
-  self.file.write(FMT.Out.File.format(
-    time,
-    self.namespace,
-    ASSOC[ord(lvl)].name,
-    info.filename,
-    info.line,
-    msg
-  ))
+  self.file.write(fmt.fileLine.format(time, self.namespace, $lvl, info.filename, info.line, msg))
 
   if self.enableConsole:
-    echo FMT.Out.Console.format(
-      time,
-      self.namespace,
-      e(ASSOC[ord(lvl)].color),
-      ASSOC[ord(lvl)].name,
-      info.filename,
-      info.line,
-      msg
-    )
+    echo fmt.consoleLine.format(time, self.namespace, e(assoc[lvl]), $lvl, info.filename, info.line, msg)
 
   if ord(lvl) > 4 and self.autoExit:
-    self.file.write(FMT.Quit.File.format(time))
+    self.file.write(fmt.fileExit.format(time))
     self.file.close()
 
     if self.enableConsole:
-      quit(FMT.Quit.Console.format(time, e(ASSOC[ord(lvl)].color)), 1)
+      quit(fmt.consoleExit.format(time, e(assoc[lvl])), 1)
     else:
       quit(1)
 
@@ -155,22 +154,20 @@ template `()`*(self: Logit, lvl: LogLevel, msg = "", quitMsg = "") =
   self.log(lvl, msg, quitMsg)
 {.pop.}
 
-# Automatically logs an error if `exp` is `false`. If autoExit is
-# `false` you may don't need/want to use this proc
+# Automatically logs an error if `exp` is `false`. If autoExit is `false` you may don't need/want to use this proc
 template test*(self: Logit, exp: bool, msg = "given expression went wrong", lvl = ERROR): untyped =
   if not exp: self.log(lvl, msg)
 
 # Writes a "header"
 proc header*(self: Logit, msg: string) =
-  let time = now().format(FMT.Time)
+  let time = now().format(fmt.time)
   
-  self.file.write(FMT.Header.File.format(time, msg))
+  self.file.write(fmt.fileHeader.format(time, msg))
 
   if self.enableConsole:
-    echo FMT.Header.Console.format(time, msg)
+    echo fmt.consoleHeader.format(time, msg)
 
-# Closes the internal file. Call this proc if you're sure you'll
-# not need to use a `Logit` instance anymore
+# Closes the internal file. Call this proc if you're sure you'll not need to use a `Logit` instance anymore
 proc done*(self: var Logit) {.inline.} =
   self.file.close()
 
