8750e609ce0175b94bf32732bc227e46425bcb65
Miqueas <miqueas2020@yahoo.com>
2023-09-17 12:23:01 -0500

    Updated test to the new release

--- test.nim
+++ test.nim
@@ -1,10 +1,12 @@
 import ./logit
 from std/os import getCurrentDir
 
-var log = initLogit(getCurrentDir(), "TEST", console = true, exit = false)
+var log = initLogit(getCurrentDir(), "TEST", logToConsole = true, exitOnError = false)
+log.start()
 
 # Uses default logging level
 log("hello!")
+
 # Uses the given logging level
 log(TRACE, "this seems to be working fine :)")
 log(INFO, "this is an info message")
@@ -17,10 +19,8 @@ TRACE.log("tracing data...")
 WARN.log("something malicious is brewing 🤨")
 
 log.header("hello there, this is a header :p")
-log.test(29 > 30, "expected 29 > 30")
-log.test(false, "expected `true`", lvl = FATAL)
 
 log(ERROR, "something went wrong!!!")
 log(FATAL, "O H   M Y   G O D N E S S")
 
-log.done()
+log.finish()
